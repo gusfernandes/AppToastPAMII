796bae1876473b75bb4ae933c58ef39ec89b2547
gusfernandes <guga.spfc.guga@gmail.com>
2021-10-25 22:15:06 -0300

    Initial commit

---
+++ AppToastPAMII/app/src/main/java/br/com/etecia/apptoastpamii/ui/login/CadastrarActivity.java
@@ -0,0 +1,38 @@
+package br.com.etecia.apptoastpamii.ui.login;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.etecia.apptoastpamii.R;
+
+public class CadastrarActivity extends AppCompatActivity {
+    EditText usuario;
+    EditText senhas;
+    Button cadastrar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cadastrar);
+        usuario = (EditText) findViewById(R.id.editusuario);
+        senhas = (EditText) findViewById(R.id.editsenhas);
+        cadastrar = (Button) findViewById(R.id.editcadastrar);
+
+        cadastrar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if (!usuario.getText().toString().isEmpty()&& !senhas.getText().toString().isEmpty()) {
+                    Toast.makeText(CadastrarActivity.this, "Usuário Cadastrado", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(CadastrarActivity.this, "Usuário não Cadastrado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
